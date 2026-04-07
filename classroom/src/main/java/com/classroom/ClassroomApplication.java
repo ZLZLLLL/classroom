4b6b158a0f634961ca0c3e20420f9aee5bbcdc2e
Le Zhang <125372422+ZLZLLLL@users.noreply.github.com>
2026-04-07 13:08:03 +0800

    Fix/about mapper (#34)
    * 修复部分错误
    
    * 考试功能
    
    * 修复部分问题
    
    * mock数据
    
    * 修改数据库相关信息，兼容er图
    
    * 修复部分bug
    
    * 修复ai部分bug
    
    * ai输出格式错误
    
    * ai输出格式错误
    
    * 更换token存储，支持ai会话历史查询，修复部分bug
    
    * 支持上传头像
    
    * 修复部分bug

--- classroom/src/main/java/com/classroom/ClassroomApplication.java
+++ classroom/src/main/java/com/classroom/ClassroomApplication.java
@@ -1,13 +1,14 @@
 package com.classroom;
 
 import com.classroom.config.TencentCosProperties;
+import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
-@MapperScan("com.classroom.repository")
+@MapperScan(basePackages = "com.classroom.repository", annotationClass = Mapper.class)
 @EnableConfigurationProperties(TencentCosProperties.class)
 @EnableScheduling
 public class ClassroomApplication {
